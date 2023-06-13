571c96bb69e04aa62cfad94a6293123860ea7c6d
Sacr1fice <sanunkurov@gmail.com>
2023-06-13 15:15:12 +0300

    ParameterizedTest

--- src/test/java/RestServiceTest.java
+++ src/test/java/RestServiceTest.java
@@ -2,23 +2,17 @@ package org.example;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class RestServiceTest {
-    @Test
-    public void shouldCalcucaleExact() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/rest.csv")
+    public void shouldCalcucaleExact(int expected, int income, int expences, int threshold) {
         RestService service = new RestService();
-        int expected = 3;
-        int actual = service.calculate(10_000, 3_000, 20_000);
+        int actual = service.calculate(income, expences, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void shouldCalcucaleExactWithAnotherData() {
-        RestService service = new RestService();
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
-
-        Assertions.assertEquals(expected, actual);
-    }
 }
