22fc250db0f798a8d2a303cd3120cf607641522d
Sacr1fice <sanunkurov@gmail.com>
2023-06-13 15:02:44 +0300

    first commit

---
+++ src/test/java/RestServiceTest.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RestServiceTest {
+    @Test
+    public void shouldCalcucaleExact() {
+        RestService service = new RestService();
+        int expected = 3;
+        int actual = service.calculate(10_000, 3_000, 20_000);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalcucaleExactWithAnotherData() {
+        RestService service = new RestService();
+        int expected = 2;
+        int actual = service.calculate(100_000, 60_000, 150_000);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
